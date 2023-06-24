45d198016088800cfe72decd89f1093f54f9d10a
ijonghyeon <academy3746@gmail.com>
2023-06-24 23:41:53 +0900

    20. Bottom Navigation Bar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,92 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  // Debug
+  final screens = [
+    const Center(
+      child: Text("Fuck U!"),
+    ),
+    const Center(
+      child: Text("Fuck U too!"),
+    ),
+    const Center(
+      child: Text("Fuck U very much!"),
+    ),
+    const Center(
+      child: Text("Fuck me harder!"),
+    ),
+    const Center(
+      child: Text("Cum inside my pussy!"),
+    ),
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTap,
+        currentIndex: _selectedIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.house,
+            ),
+            label: "Home",
+            tooltip: "Who are you?",
+            backgroundColor: Colors.amber,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+            label: "Search",
+            tooltip: "What the fuck?",
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.house,
+            ),
+            label: "Home",
+            tooltip: "Who are you?",
+            backgroundColor: Colors.pink,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+            ),
+            label: "Search",
+            tooltip: "What the fuck?",
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.house,
+            ),
+            label: "Home",
+            tooltip: "Who are you?",
+            backgroundColor: Colors.grey,
+          ),
+        ],
+        //selectedItemColor: Theme.of(context).primaryColor,
+      ),
+    );
   }
 }
