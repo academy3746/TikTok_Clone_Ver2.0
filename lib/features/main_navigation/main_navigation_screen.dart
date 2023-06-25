8a625f5494998053cd180a9f730b674315ca6c87
ijonghyeon <academy3746@gmail.com>
2023-06-25 14:10:23 +0900

    22. Cupertino Tab Bar

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/cupertino.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -9,7 +8,7 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
-  int _selectedIndex = 0;
+  //int _selectedIndex = 0;
 
   // Debug
   final screens = [
@@ -30,37 +29,34 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     ),
   ];
 
+  /*
   void _onTap(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
+  */
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: _onTap,
-        destinations: const [
-          NavigationDestination(
-            icon: FaIcon(
-              FontAwesomeIcons.house,
-              color: Colors.white,
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.house,
             ),
             label: "Home",
           ),
-          NavigationDestination(
-            icon: FaIcon(
-              FontAwesomeIcons.magnifyingGlass,
-              color: Colors.white,
+          BottomNavigationBarItem(
+            icon: Icon(
+              CupertinoIcons.search,
             ),
             label: "Search",
           ),
         ],
       ),
+      tabBuilder: (BuildContext context, int index) => screens[index],
     );
   }
 }
