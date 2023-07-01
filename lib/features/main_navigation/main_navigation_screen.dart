acd42792660bd26a7014024cdbf70edb8129ea13
ijonghyeon <academy3746@gmail.com>
2023-07-01 16:29:22 +0900

    24. Post Video Button

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 import 'package:tiktok/features/main_navigation/widgets/nav_tab.dart';
+import 'package:tiktok/features/main_navigation/widgets/post_video_button.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -16,19 +18,44 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   // Debug
   final screens = [
     const Center(
-      child: Text("Fuck U!"),
+      child: Text(
+        "HOME",
+        style: TextStyle(
+          fontSize: 49,
+        ),
+      ),
     ),
     const Center(
-      child: Text("Fuck U too!"),
+      child: Text(
+        "DISCOVER",
+        style: TextStyle(
+          fontSize: 49,
+        ),
+      ),
     ),
     const Center(
-      child: Text("Fuck U very much!"),
+      child: Text(
+        "UPLOAD",
+        style: TextStyle(
+          fontSize: 49,
+        ),
+      ),
     ),
     const Center(
-      child: Text("Fuck me harder!"),
+      child: Text(
+        "INBOX",
+        style: TextStyle(
+          fontSize: 49,
+        ),
+      ),
     ),
     const Center(
-      child: Text("Cum inside my pussy!"),
+      child: Text(
+        "PROFILE",
+        style: TextStyle(
+          fontSize: 49,
+        ),
+      ),
     ),
   ];
 
@@ -38,9 +65,27 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     });
   }
 
+  void _onTapVideoButtonTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        // Render Camera Widget later
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Record Video",
+            ),
+          ),
+        ),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // body: screens[_selectedIndex],
+      body: screens.elementAt(_selectedIndex),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Padding(
@@ -54,30 +99,34 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 text: "HOME",
                 isSelected: _selectedIndex == 0,
                 icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
                 onTap: () => _onTap(0),
               ),
               NavTab(
                 text: "DISCOVER",
                 isSelected: _selectedIndex == 1,
-                icon: FontAwesomeIcons.magnifyingGlass,
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
                 onTap: () => _onTap(1),
               ),
-              NavTab(
-                text: "UPLOAD",
-                isSelected: _selectedIndex == 2,
-                icon: FontAwesomeIcons.plus,
-                onTap: () => _onTap(2),
+              Gaps.h24,
+              GestureDetector(
+                onTap: _onTapVideoButtonTap,
+                child: const PostVideoButton(),
               ),
+              Gaps.h24,
               NavTab(
                 text: "INBOX",
                 isSelected: _selectedIndex == 3,
                 icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
                 onTap: () => _onTap(3),
               ),
               NavTab(
                 text: "PROFILE",
                 isSelected: _selectedIndex == 4,
                 icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
                 onTap: () => _onTap(4),
               ),
             ],
