0204d21f32f99c0ddd79d2af3f20c951fabf6301
ijonghyeon <academy3746@gmail.com>
2023-07-13 21:19:08 +0900

    42. Grid Item2

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 final tabs = ["Top", "Users", "Videos", "Sounds", "LIVE", "Shopping", "Brands"];
@@ -38,23 +40,74 @@ class DiscoverScreen extends StatelessWidget {
         body: TabBarView(
           children: [
             GridView.builder(
-              itemCount: 20,
-              padding: const EdgeInsets.all(
-                Sizes.size6,
-              ),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                childAspectRatio: 9 / 16,
-                crossAxisCount: 2,
-                crossAxisSpacing: Sizes.size10,
-                mainAxisSpacing: Sizes.size10,
-              ),
-              itemBuilder: (BuildContext context, int index) => Container(
-                color: Colors.teal,
-                child: Center(
-                  child: Text("$index"),
+                itemCount: 20,
+                padding: const EdgeInsets.all(
+                  Sizes.size6,
                 ),
-              ),
-            ),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 9 / 20,
+                  crossAxisCount: 2,
+                  crossAxisSpacing: Sizes.size10,
+                  mainAxisSpacing: Sizes.size10,
+                ),
+                itemBuilder: (BuildContext context, int index) => Column(
+                      children: [
+                        AspectRatio(
+                          aspectRatio: 9 / 16,
+                          child: FadeInImage.assetNetwork(
+                            fit: BoxFit.cover,
+                            placeholder: "assets/images/shogun.webp",
+                            image:
+                                "https://ac-p1.namu.la/20230417sac/1cd216f5235d5ae6f8f3c88ae811186140600ea9d47201bc5a1ec4a501639267.png?expires=1689252525&key=-R2DBoGvRXPsUhIpRYb3hQ",
+                          ),
+                        ),
+                        Gaps.v10,
+                        const Text(
+                          "Ride the tiger! you can see his stripes but you know he's clean.",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: Sizes.size18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Gaps.v8,
+                        DefaultTextStyle(
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          child: Row(
+                            children: [
+                              const CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/107133642?v=4",
+                                ),
+                                radius: 12,
+                              ),
+                              Gaps.h4,
+                              const Expanded(
+                                child: Text(
+                                  "One for the money, one for the show! round & round & round we go!",
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ),
+                              Gaps.h4,
+                              FaIcon(
+                                FontAwesomeIcons.heart,
+                                size: Sizes.size16,
+                                color: Colors.grey.shade600,
+                              ),
+                              Gaps.h2,
+                              const Text(
+                                "2.5M",
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )),
             for (var tab in tabs.skip(1))
               Center(
                 child: Text(
