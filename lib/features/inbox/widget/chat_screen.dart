89f03efd0aef31f798d451398ccc0f8e643adb91
ijonghyeon <academy3746@gmail.com>
2023-07-19 15:01:07 +0900

    52. AnimatedList

--- lib/features/inbox/widget/chat_screen.dart
+++ lib/features/inbox/widget/chat_screen.dart
@@ -10,6 +10,22 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(
+          milliseconds: 300,
+        ),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,15 +34,65 @@ class _ChatScreenState extends State<ChatScreen> {
         title: const Text("Direct Messages"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(
               FontAwesomeIcons.plus,
             ),
           ),
         ],
       ),
-      //Dummy Widget
-      body: ListView(
+      body: AnimatedList(
+        key: _key,
+        padding: const EdgeInsets.symmetric(
+          vertical: Sizes.size10,
+        ),
+        itemBuilder:
+            (BuildContext context, int index, Animation<double> animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 28,
+                  foregroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/107133642?v=4"),
+                  child: Text(
+                    "DIO",
+                  ),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Ronnie James Dio ($index)",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "13:57",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: const Text("Don't talk to stranger..!"),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// Dummy
+/*body: ListView(
         padding: const EdgeInsets.symmetric(
           vertical: Sizes.size10,
         ),
@@ -60,7 +126,4 @@ class _ChatScreenState extends State<ChatScreen> {
             subtitle: const Text("Don't talk to stranger..!"),
           ),
         ],
-      ),
-    );
-  }
-}
+      ),*/
