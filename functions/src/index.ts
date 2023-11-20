c44425b4c7ce06a45a76f2cc9627db0900347def
ijonghyeon <academy3746@gmail.com>
2023-11-21 00:09:06 +0900

    133. 개씹좆같은 node.js

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,6 +10,7 @@
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -20,5 +21,25 @@ export const helloWorld = onRequest((request, response) => {
 });
 
 export const onVideoCreated = functions.firestore.document("videos/{videoId}").onCreate(async (snapshot, context) => {
-  await snapshot.ref.update({hello: "Hello, Firebase!"});
+  const spawn = require("child-process-promise").spawn;
+
+  const video = snapshot.data();
+
+  await spawn("ffmpeg", [
+    "-i",
+    video.fileUrl,
+    "-ss",
+    "00:00:01.000",
+    "-vframes",
+    "1",
+    "-vf",
+    "scale=150:-1",
+    `/tmp/${snapshot.id}.jpg`,
+  ]);
+
+  const storage = admin.storage();
+
+  await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+    destination: `thumbnails/${snapshot.id}.jpg`,
+  });
 });
