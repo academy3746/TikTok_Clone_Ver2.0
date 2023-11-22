5a0388bfa4f42d22ba76bc7294f0ffc352c93b35
ijonghyeon <academy3746@gmail.com>
2023-11-22 23:57:38 +0900

    137. 좀 수정..

--- lib/features/videos/view_models/video_post_vm.dart
+++ lib/features/videos/view_models/video_post_vm.dart
@@ -6,11 +6,11 @@ import 'package:tiktok/features/videos/repo/videos_repo.dart';
 class VideoPostViewModel extends FamilyAsyncNotifier<void, String> {
   late final VideosRepository _repository;
 
-  late final _videoId;
+  late final String _videoId;
 
   @override
-  FutureOr build(String videoId) {
-    _videoId = videoId;
+  FutureOr build(String arg) {
+    _videoId = arg;
 
     _repository = ref.read(videoRepo);
   }
