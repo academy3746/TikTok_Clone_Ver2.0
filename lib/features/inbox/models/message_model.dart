173de8d35c88be0f0f48a1f0b3f0e89844be5920
ijonghyeon <academy3746@gmail.com>
2023-11-27 22:00:06 +0900

    143. Stream Provider

--- lib/features/inbox/models/message_model.dart
+++ lib/features/inbox/models/message_model.dart
@@ -3,19 +3,24 @@ class MessageModel {
 
   final String userId;
 
+  final int datetime;
+
   MessageModel({
     required this.text,
     required this.userId,
+    required this.datetime,
   });
 
   MessageModel.fromJson(Map<String, dynamic> json)
       : text = json["text"],
-        userId = json["userId"];
+        userId = json["userId"],
+        datetime = json["datetime"];
 
   Map<String, dynamic> toJson() {
     return {
       "text": text,
       "userId": userId,
+      "datetime": datetime,
     };
   }
 }
--- lib/features/inbox/view_models/dm_vm.dart
+++ lib/features/inbox/view_models/dm_vm.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok/features/auth/repo/auth_repo.dart';
 import 'package:tiktok/features/inbox/models/message_model.dart';
@@ -22,6 +23,7 @@ class DirectMessageViewModel extends AsyncNotifier<void> {
         final msg = MessageModel(
           text: text,
           userId: user!.uid,
+          datetime: DateTime.now().millisecondsSinceEpoch,
         );
 
         _repository.sendDM(msg);
@@ -33,3 +35,23 @@ class DirectMessageViewModel extends AsyncNotifier<void> {
 final dmProvider = AsyncNotifierProvider<DirectMessageViewModel, void>(
   () => DirectMessageViewModel(),
 );
+
+final chatProvider = StreamProvider<List<MessageModel>>((ref) {
+  final db = FirebaseFirestore.instance;
+
+  return db
+      .collection("chat_rooms")
+      .doc("bmItwGjK4PqmHHb2yjz6")
+      .collection("texts")
+      .orderBy("datetime")
+      .snapshots()
+      .map(
+        (event) => event.docs
+            .map(
+              (doc) => MessageModel.fromJson(
+                doc.data(),
+              ),
+            )
+            .toList(),
+      );
+});
